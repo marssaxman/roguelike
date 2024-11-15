7d53f6f3a95db55ff40a4a9a9419da58994ddb96
Mars Saxman <github@marssaxman.com>
2024-11-14 21:23:53 -0800

    clean up: check for venv existence before launch, update libtocd api

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ DIRECTORY=`dirname $0`
 
 if [ -d "$DIRECTORY/venv" ]; then
     source venv/bin/activate
+else
+    echo "Python virtual environment does not exist!"
+    echo "Please execute build.sh before running."
+    exit 1
 fi;
 
 PYTHONPATH=$DIRECTORY python3 $DIRECTORY/src/main.py "$@"
