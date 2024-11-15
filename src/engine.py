0835016f6960fe267d92b15959007038e3bfc43c
Mars Saxman <github@marssaxman.com>
2024-11-15 15:42:50 -0800

    tutorial part 4: add field-of-view and previously-explored state

--- src/engine.py
+++ src/engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -14,23 +15,31 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
             action = self.event_handler.dispatch(event)
-
             if action is None:
                 continue
-
             action.perform(self, self.player)
+            self.update_fov()
 
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the player's point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is visible, add it to the "explored" map.
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
-
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
-
+            # Only print entities which are currently visible
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
         context.present(console)
-
         console.clear()
