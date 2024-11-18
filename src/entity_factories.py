e58dcb5590d025219dceaab13617974ef564aae1
Mars Saxman <github@marssaxman.com>
2024-11-17 21:44:10 -0800

    tutorial part 6: enemy AI and combat

--- src/entity_factories.py
+++ src/entity_factories.py
@@ -1,7 +1,36 @@
-from entity import Entity
+from components.ai import HostileEnemy, Passive
+from components.fighter import Fighter
+from entity import Actor
 
-player = Entity(char="@", color=(255,255,255), name="Player", blocks_movement=True)
+player = Actor(
+    char="@",
+    color=(255, 255, 255),
+    name="Player",
+    ai_cls=Passive,
+    fighter=Fighter(hp=30, defense=2, power=5),
+)
 
-orc = Entity(char="o", color=(63,127,64), name="Orc", blocks_movement=True)
-troll = Entity(char="T", color=(0,127,0), name="Troll", blocks_movement=True)
-rat = Entity(char="r", color=(127,127,200), name="Rat", blocks_movement=True)
+orc = Actor(
+    char="o",
+    color=(63, 127, 63),
+    name="Orc",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
+)
+
+troll=Actor(
+    char="T",
+    color=(0, 127, 0),
+    name="Troll",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+)
+
+rat=Actor(
+    char="r",
+    color=(127, 127, 200),
+    name="Rat",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=4, defense=0, power=1),
+)
+
