9e44bd7f5d3bcadb7d72482ab49638e16f81eef5
Mars Saxman <github@marssaxman.com>
2024-11-17 22:01:31 -0800

    tutorial part 6: player death

--- src/input_handlers.py
+++ src/input_handlers.py
@@ -52,6 +52,14 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         self.engine = engine
 
     def handle_events(self) -> None:
+        raise NotImplementedError()
+
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+
+class MainGameEventHandler(EventHandler):
+    def handle_events(self) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
             if action is None:
@@ -59,9 +67,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             action.perform()
             self.engine.handle_enemy_turns()
             self.engine.update_fov()
-
-    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
-        raise SystemExit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
@@ -79,3 +84,21 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         # No valid key was pressed
         return action
+
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+            if action is None:
+                continue
+            action.perform()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+        key = event.sym
+        if key == tcod.event.KeySym.ESCAPE:
+            action = EscapeAction(self.engine.player)
+        #No valid key was pressed
+        return action
+
