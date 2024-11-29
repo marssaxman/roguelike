f8b86ad5e3606e5d1ca4e4b136823db95ad61cad
Mars Saxman <github@marssaxman.com>
2024-11-29 00:14:23 -0800

    unique tile types for each wall orientation

--- src/maze/render.py
+++ src/maze/render.py
@@ -8,79 +8,75 @@ from typing import Any
 from .basemap import Tile
 import codepoint as cp
 
-# We will look up the appropriate wall character by generating a bit mask
-# representing the neighboring top, left, bottom, and right squares: each one
-# which contains another wall will have its bit set. We can then sum the
-# adjacent items and return the appropriate character.
-WALL_GLYPHS = [# L T R B
-    cp.COLUMN,    # . . . .
-    cp.WALL_B,    # . . . X
-    cp.WALL_R,    # . . X .
-    cp.WALL_RB,   # . . X X
-    cp.WALL_A,    # . X . .
-    cp.WALL_AB,   # . X . X
-    cp.WALL_AR,   # . X X .
-    cp.WALL_ARB,  # . X X X
-    cp.WALL_L,    # X . . .
-    cp.WALL_LB,   # X . . X
-    cp.WALL_LR,   # X . X .
-    cp.WALL_LRB,  # X . X X
-    cp.WALL_LA,   # X X . .
-    cp.WALL_LAB,  # X X . X
-    cp.WALL_LAR,  # X X X .
-    cp.WALL_LARB, # X X X X
-]
-
-
-def place_door(grid, x, y):
-    # If there are walls above and below, return a vertical door.
-    # If there are walls left and right, return a horizontal door.
-    # Otherwise - this should never happen - return a plus sign.
-    if y > 0 and (y+1) < grid.shape[1]:
-        if grid[x, y-1] == Tile.WALL and grid[x, y+1] == Tile.WALL:
-            return cp.DOOR_V
-    if x > 0 and (x+1) < grid.shape[0]:
-        if grid[x-1, y] == Tile.WALL and grid[x+1, y] == Tile.WALL:
-            return cp.DOOR_H
-    return cp.PLUS_SIGN
-
-
-def place_wall(grid, x: np.uint, y:np.uint):
-    wall_left = x > 0 and grid[x-1, y] == Tile.WALL
-    wall_above = y > 0 and grid[x, y-1] == Tile.WALL
-    wall_right = (x+1) < grid.shape[0] and grid[x+1, y] == Tile.WALL
-    wall_below = (y+1) < grid.shape[1] and grid[x, y+1] == Tile.WALL
-    index = 0
-    index += 8 if wall_left else 0
-    index += 4 if wall_above else 0
-    index += 2 if wall_right else 0
-    index += 1 if wall_below else 0
-    return WALL_GLYPHS[index]
-
-
-def to_chars(grid):
-    maze = np.full(shape=grid.shape, dtype=np.uint32, fill_value=cp.SPACE)
-    for x, y in np.ndindex(maze.shape):
-        tile = grid[x, y]
-        char = 0
-        if tile == Tile.VOID:
-            char = cp.SPACE
-        elif tile == Tile.FLOOR:
-            char = cp.FULL_STOP
-        elif tile == Tile.DOOR:
-            char = place_door(grid, x, y)
-        elif tile == Tile.WALL:
-            char = place_wall(grid, x, y)
-        maze[x, y] = char
-    return maze
-
 
 @dataclass
 class Palette:
     void: Any
     floor: Any
     door: Any
+    door_H: Any
+    door_V: Any
     wall: Any
+    wall_B: Any
+    wall_R: Any
+    wall_RB: Any
+    wall_A: Any
+    wall_AB: Any
+    wall_AR: Any
+    wall_ARB: Any
+    wall_L: Any
+    wall_LB: Any
+    wall_LR: Any
+    wall_LRB: Any
+    wall_LA: Any
+    wall_LAB: Any
+    wall_LAR: Any
+    wall_LARB: Any
+
+
+def place_door(grid, x, y, palette: Palette):
+    # If there are walls above and below, return a vertical door.
+    # If there are walls left and right, return a horizontal door.
+    # Otherwise - this should never happen - return a plus sign.
+    if y > 0 and (y+1) < grid.shape[1]:
+        if grid[x, y-1] == Tile.WALL and grid[x, y+1] == Tile.WALL:
+            return palette.door_V
+    if x > 0 and (x+1) < grid.shape[0]:
+        if grid[x-1, y] == Tile.WALL and grid[x+1, y] == Tile.WALL:
+            return palette.door_H
+    return palette.door
+
+
+def place_wall(grid, x: np.uint, y:np.uint, palette: Palette):
+    wall_left = x > 0 and grid[x-1, y] == Tile.WALL
+    wall_above = y > 0 and grid[x, y-1] == Tile.WALL
+    wall_right = (x+1) < grid.shape[0] and grid[x+1, y] == Tile.WALL
+    wall_below = (y+1) < grid.shape[1] and grid[x, y+1] == Tile.WALL
+    glyphs = [             # L A R B
+        palette.wall,      # . . . .
+        palette.wall_B,    # . . . X
+        palette.wall_R,    # . . X .
+        palette.wall_RB,   # . . X X
+        palette.wall_A,    # . X . .
+        palette.wall_AB,   # . X . X
+        palette.wall_AR,   # . X X .
+        palette.wall_ARB,  # . X X X
+        palette.wall_L,    # X . . .
+        palette.wall_LB,   # X . . X
+        palette.wall_LR,   # X . X .
+        palette.wall_LRB,  # X . X X
+        palette.wall_LA,   # X X . .
+        palette.wall_LAB,  # X X . X
+        palette.wall_LAR,  # X X X .
+        palette.wall_LARB, # X X X X
+    ]
+    index = 0
+    index += 8 if wall_left else 0
+    index += 4 if wall_above else 0
+    index += 2 if wall_right else 0
+    index += 1 if wall_below else 0
+    return glyphs[index]
+
 
 def tiles(src, dest, palette: Palette):
     assert len(src.shape) == 2 and len(dest.shape) == 2
@@ -93,8 +89,37 @@ def tiles(src, dest, palette: Palette):
         elif src_val == Tile.FLOOR:
             dest_val = palette.floor
         elif src_val == Tile.DOOR:
-            dest_val = palette.door
+            dest_val = place_door(src, x, y, palette)
         elif src_val == Tile.WALL:
-            dest_val = palette.wall
+            dest_val = place_wall(src, x, y, palette)
         dest[x, y] = dest_val
+
+
+def to_chars(grid):
+    maze = np.full(shape=grid.shape, dtype=np.uint32, fill_value=cp.SPACE)
+    palette = Palette(
+        void=cp.VOID,
+        floor=cp.FLOOR,
+        door=cp.DOOR,
+        door_H=cp.DOOR_H,
+        door_V=cp.DOOR_V,
+        wall=cp.COLUMN,
+        wall_B=cp.WALL_B,
+        wall_R=cp.WALL_R,
+        wall_RB=cp.WALL_RB,
+        wall_A=cp.WALL_A,
+        wall_AB=cp.WALL_AB,
+        wall_AR=cp.WALL_AR,
+        wall_ARB=cp.WALL_ARB,
+        wall_L=cp.WALL_L,
+        wall_LB=cp.WALL_LB,
+        wall_LR=cp.WALL_LR,
+        wall_LRB=cp.WALL_LRB,
+        wall_LA=cp.WALL_LA,
+        wall_LAB=cp.WALL_LAB,
+        wall_LAR=cp.WALL_LAR,
+        wall_LARB=cp.WALL_LARB,
+    )
+    tiles(grid, maze, palette)
+    return maze
 
--- src/tile_types.py
+++ src/tile_types.py
@@ -33,6 +33,23 @@ def new_tile(
     """Helper function for defining individual tile types """
     return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+def new_wall(char_id):
+    return new_tile(
+        walkable=False,
+        transparent=False,
+        dark=(char_id, (127, 127, 127), (0, 0, 100)),
+        light=(char_id, (255, 255, 255), (130, 110, 50)),
+    )
+
+def new_door(char_id):
+    return new_tile(
+        walkable=True,
+        transparent=False,
+        dark=(char_id, (127, 127, 127), (0, 0, 100)),
+        light=(char_id, (255, 255, 255), (165, 145, 50)),
+    )
+
+
 # Shroud represents tiles which have not yet been explored
 SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
@@ -43,19 +60,27 @@ floor = new_tile(
     light=(cp.SPACE, (255, 255, 255), (200, 180, 50)),
 )
 
-wall = new_tile(
-    walkable=False,
-    transparent=False,
-    dark=(cp.WALL, (127, 127, 127), (0, 0, 100)),
-    light=(cp.WALL, (255, 255, 255), (130, 110, 50)),
-)
+door = new_door(cp.DOOR)
+door_H = new_door(cp.DOOR_H)
+door_V = new_door(cp.DOOR_V)
+wall = new_wall(cp.WALL)
+wall_L = new_wall(cp.WALL_L)
+wall_A = new_wall(cp.WALL_A)
+wall_R = new_wall(cp.WALL_R)
+wall_B = new_wall(cp.WALL_B)
+wall_LR = new_wall(cp.WALL_LR)
+wall_AB = new_wall(cp.WALL_AB)
+wall_RB = new_wall(cp.WALL_RB)
+wall_LB = new_wall(cp.WALL_LB)
+wall_AR = new_wall(cp.WALL_AR)
+wall_LA = new_wall(cp.WALL_LA)
+wall_ARB = new_wall(cp.WALL_ARB)
+wall_LAB = new_wall(cp.WALL_LAB)
+wall_LRB = new_wall(cp.WALL_LRB)
+wall_LAR = new_wall(cp.WALL_LAR)
+wall_LARB = new_wall(cp.WALL_LARB)
 
-door = new_tile(
-    walkable=True,
-    transparent=False,
-    dark=(cp.DOOR, (127, 127, 127), (0, 0, 100)),
-    light=(cp.DOOR, (255, 255, 255), (165, 145, 50)),
-)
+
 
 down_stairs = new_tile(
     walkable=True,
