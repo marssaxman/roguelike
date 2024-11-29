b7f6f3a1549c84f0e67786efdd93c723ca0e5ad3
Mars Saxman <github@marssaxman.com>
2024-11-29 01:47:51 -0800

    simple implementation of graphic walls using DawnLike tile set

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import setup_game
 import input_handlers
 from entity import Entity
 from random import randrange
+import codepoint
 
 
 def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
@@ -15,16 +16,55 @@ def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
         handler.engine.save_as(filename)
         print("Game saved.")
 
+def load_tiles():
+    # load the main font
+    tileset = tcod.tileset.load_tilesheet(
+        "assets/Cooz_curses_square_16x16.png", 16, 16, tcod.tileset.CHARMAP_CP437
+    )
+
+    # load some graphics
+    wall_tiles = tcod.tileset.load_tilesheet(
+        "assets/DawnLike/Objects/Wall.png", (320//16), (816//16), range(1020)
+    )
+    # 20 tiles in a row, 51 rows
+    # Wall tiles exist in a 6x3 cluster with 1 space between columns
+    # Clusters exist in a 4-high group (6x12 tiles) of color variants
+    # We'll use the brick cluster in row 1
+    # Within a cluster, tiles are located like thus:
+    # RB    LR    LB    #       LRB
+    # AB    A           ARB     LARB    LAB
+    # AR          LA            LAR
+    # (B = AB, L = AL, R=AR)
+    tileset.set_tile(codepoint.WALL, wall_tiles.get_tile(63))
+    tileset.set_tile(codepoint.WALL_L, wall_tiles.get_tile(61))
+    tileset.set_tile(codepoint.WALL_A, wall_tiles.get_tile(81))
+    tileset.set_tile(codepoint.WALL_R, wall_tiles.get_tile(61))
+    tileset.set_tile(codepoint.WALL_B, wall_tiles.get_tile(80))
+    tileset.set_tile(codepoint.WALL_LR, wall_tiles.get_tile(61))
+    tileset.set_tile(codepoint.WALL_AB, wall_tiles.get_tile(80))
+    tileset.set_tile(codepoint.WALL_RB, wall_tiles.get_tile(60))
+    tileset.set_tile(codepoint.WALL_LB, wall_tiles.get_tile(62))
+    tileset.set_tile(codepoint.WALL_AR, wall_tiles.get_tile(100))
+    tileset.set_tile(codepoint.WALL_LA, wall_tiles.get_tile(102))
+    tileset.set_tile(codepoint.WALL_ARB, wall_tiles.get_tile(83))
+    tileset.set_tile(codepoint.WALL_LAB, wall_tiles.get_tile(85))
+    tileset.set_tile(codepoint.WALL_LRB, wall_tiles.get_tile(64))
+    tileset.set_tile(codepoint.WALL_LAR, wall_tiles.get_tile(104))
+    tileset.set_tile(codepoint.WALL_LARB, wall_tiles.get_tile(84))
+
+    door_tiles = tcod.tileset.load_tilesheet(
+        "assets/DawnLike/Objects/Door0.png", 8, 6, range(48)
+    )
+    tileset.set_tile(codepoint.DOOR, door_tiles.get_tile(0))
+
+    return tileset
+
 def main():
     # how big should the game window be?
     screen_width = 80
     screen_height = 50
 
-    # load the graphics
-    tileset = tcod.tileset.load_tilesheet(
-        "assets/Cooz_curses_square_16x16.png", 16, 16, tcod.tileset.CHARMAP_CP437
-    )
-
+    tileset = load_tiles()
     handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
     # create a terminal window to put the game interface in
