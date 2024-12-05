36240fdf669f203eb9e87b2ecdbc4ed83135a693
Mars Saxman <github@marssaxman.com>
2024-12-04 17:25:39 -0800

    better internal api for room raster grids, using ndarray views

--- src/maze/create.py
+++ src/maze/create.py
@@ -24,24 +24,29 @@ def _coarse_grid(
         yield left * 2, top * 2, right * 2, bottom * 2
 
 
-def _rasterize(width: np.uint, height: np.uint, rects):
+def _rasterize(rects, grid):
     # Allocate an extra row and column, which will be left empty;
     # this means we can skip the range checks when scanning 2x2 boxes.
-    shape = (np.add(width, 1, dtype=np.uint), np.add(height, 1, dtype=np.uint))
-    grid = np.zeros(shape, dtype=np.uint)
+    width = grid.shape[0]
+    height = grid.shape[1]
     counter = 0
     for rect in rects:
         left, top, right, bottom = rect
         # Skip rects that don't lie completely within the requested area
-        if left <= 0 or top <= 0 or right > width or bottom > height:
+        if left < 0 or top < 0 or right > width or bottom > height:
             continue;
         # Increment the room counter and assign it to the cell region
         counter += 1
         grid[left:right, top:bottom] = counter
-    return grid
 
 
 def _apply_grid(grid, builder):
+    """Generate map tiles from a raster grid containing room numbers."""
+    # Each intersection of four grid cells yields one map tile, so we expect
+    # that the grid's width and height are at least one greater than the
+    # resulting map dimensions.
+    assert grid.shape[0] > builder.shape[0]
+    assert grid.shape[1]
     for x, y in np.ndindex(builder.shape):
         # Get the four room indexes touching this grid junction
         tl = grid[x, y]
@@ -57,28 +62,35 @@ def _apply_grid(grid, builder):
         else:
             builder.place_pillar(x, y)
 
+
 def level(
-    width: np.uint,
-    height: np.uint,
+    width: np.uint, # minimum 8
+    height: np.uint, # minimum 8
     box_size: np.uint = np.uint(8), # minimum 3
     rng: np.random.Generator = np.random.default_rng(),
 ) -> basemap.BaseMap:
     """Generate a basemap for a level having the specified dimensions."""
+    assert box_size >= 3
+    assert width >= 8 and height >= 8
     # Generate offset grid rectangles which will cover the game area.
-    # We will use half the requested width and height, ensuring that every
-    # rect has a minimum dimension 2, which we need in order to line each
-    # edge of the room with a wall tile.
-    assert box_size > 2
+    # We will use half the tile grid resolution, to ensure that every rect
+    # has enough open area to accommodate wall tiles along its edges.
+    # We will also allocate a raster grid one unit larger than the desired
+    # map grid, then render into the center of this array, leaving one space
+    # empty around every edge. That way we can convert each 2x2 group into
+    # one map tile, overscanning the edges, to generate perimeter walls.
+    grid = np.zeros((np.uint(width+1), np.uint(height+1)), dtype=np.uint)
+    inset = grid[1:np.uint(width-1), 1:np.uint(height-1)]
     grid_seed = offgrid.hash64(rng.integers(0xFFFFFFFF))
     rects = _coarse_grid(
-        width=width,
-        height=height,
+        width=inset.shape[0],
+        height=inset.shape[1],
         box_size=box_size,
         seed=np.int64(grid_seed),
         edge=0.15
     )
     assert rects
-    grid = _rasterize(width, height, rects)
+    _rasterize(rects, inset)
     builder = basemap.Builder(width, height)
     _apply_grid(grid, builder)
     # The grid squares have become isolated rooms, separated by walls.
@@ -87,6 +99,16 @@ def level(
     connect.some(builder=builder, rng=rng)
     connect.corridors(builder=builder)
     return builder.build()
+
+
+def _print_grid(caption, grid):
+    CHARS = ".123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    print(caption)
+    for y in range(grid.shape[1]):
+        line = ""
+        for x in range(grid.shape[0]):
+            line += CHARS[grid[x, y]]
+        print(line)
 
 
 def _mask_rects(rects, mask):
@@ -99,19 +121,22 @@ def _mask_rects(rects, mask):
         if np.any(mask[left:right, top:bottom]):
             yield left, top, right, bottom
 
-def _lair_mask(width, height):
+
+def _lair_mask(grid):
     # The topmost floor contains the Lair of Bob, which must cover at least
     # the centermost 8x8 tiles of the game map.
     # Allocate one extra row and column for compatibility with _rasterize
+    width = grid.shape[0]
+    height = grid.shape[1]
     center_x = np.floor_divide(width, 2, dtype=np.uint)
-    center_y = np.floor_divide(height, 2, dtype=np.uint)
+    center_y = np.floor_divide(np.uint(height+1), 2, dtype=np.uint)
     mask_l = np.subtract(center_x, 4, dtype=np.uint)
     mask_r = np.add(center_x, 4, dtype=np.uint)
     mask_t = np.subtract(center_y, 4, dtype=np.uint)
     mask_b = np.add(center_y, 4, dtype=np.uint)
     assert mask_l >= 0 and mask_t >= 0
     assert mask_r <= width and mask_b <= height
-    return _rasterize(width, height, [(mask_l, mask_t, mask_r, mask_b)])
+    _rasterize([(mask_l, mask_t, mask_r, mask_b)], grid)
 
 
 def tower(
@@ -123,25 +148,38 @@ def tower(
 ) -> List[basemap.BaseMap]:
 
     # Initial mask: centered area large enough for Lair of Bob
-    mask = _lair_mask(width, height)
+    mask = np.zeros((np.uint(width+1), np.uint(height+1)), dtype=np.uint)
+    _lair_mask(mask)
+    _print_grid("Initial mask", mask)
     # Populate a list of maps, from top down (?)
     maps = list()
     for level in range(stories):
+        # prepare the buffer we will write the new level into
+        grid = np.zeros_like(mask)
+        inset = grid[1:np.uint(width-1), 1:np.uint(height-1)]
+        # generate an offset grid
         grid_seed = offgrid.hash64(rng.integers(0xFFFFFFFF))
         rects = _coarse_grid(
-            width=width,
-            height=height,
+            width=inset.shape[0],
+            height=inset.shape[1],
             box_size=6,
             seed=np.int64(grid_seed),
             edge=0.075
         )
         assert rects
-        grid = _rasterize(width, height, _mask_rects(rects, mask))
+        _rasterize(_mask_rects(rects, mask), inset)
+        _print_grid("Rasterized rooms", grid)
         builder = basemap.Builder(width, height)
         _apply_grid(grid, builder)
         connect.fully(builder=builder, rng=rng)
         connect.some(builder=builder, rng=rng)
         connect.corridors(builder=builder)
         maps.append(builder.build())
-        mask += grid
+        new_mask = np.zeros_like(mask)
+        new_mask[grid.nonzero()] = 1
+        _print_grid("Next mask", new_mask)
+        error = mask - (new_mask * mask)
+        if np.any(error):
+            _print_grid("Error:", error)
+        mask = new_mask
     return maps
