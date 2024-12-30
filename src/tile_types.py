82fefdca6b60c36787315ae92a7b5880e5c8a8fa
Mars Saxman <github@marssaxman.com>
2024-12-29 19:51:08 -0800

    shadowed tile types are slightly blue

--- src/tile_types.py
+++ src/tile_types.py
@@ -3,6 +3,7 @@ import numpy as np  # type: ignore
 from functools import cache
 
 import graphics
+import color
 
 # Tile graphics structured type compatible with Console.tiles_rgb.
 graphic_dt = np.dtype(
@@ -38,8 +39,8 @@ def wall(char_id):
     return new_tile(
         walkable=False,
         transparent=False,
-        dark=(char_id, (127, 127, 127), (0, 0, 0)),
-        light=(char_id, (255, 255, 255), (0, 0, 0)),
+        dark=(char_id, (95, 95, 127), color.black),
+        light=(char_id, color.white, color.black),
     )
 
 @cache
@@ -47,8 +48,8 @@ def door(char_id):
     return new_tile(
         walkable=True,
         transparent=False,
-        dark=(char_id, (127, 127, 127), (0, 0, 0)),
-        light=(char_id, (255, 255, 255), (0, 0, 0)),
+        dark=(char_id, (95, 95, 127), color.black),
+        light=(char_id, color.white, color.black),
     )
 
 @cache
@@ -56,32 +57,32 @@ def floor(char_id):
     return new_tile(
         walkable=True,
         transparent=True,
-        dark=(char_id, (127, 127, 127), (0, 0, 0)),
-        light=(char_id, (255, 255, 255), (0, 0, 0)),
+        dark=(char_id, (95, 95, 127), color.black),
+        light=(char_id, color.white, color.black),
     )
 
 # Shroud represents tiles which have not yet been explored
-SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
+SHROUD = np.array((ord(" "), color.white, color.black), dtype=graphic_dt)
 
 # Map initialization value used prior to style painting
 DEFAULT = new_tile(
     walkable=False,
     transparent=False,
     dark=(ord("#"), (127, 127, 127), (50, 50, 150)),
-    light=(ord("#"), (255, 255, 255), (200, 180, 50)),
+    light=(ord("#"), color.white, (200, 180, 50)),
 )
 
 exit_stairs = new_tile(
     walkable=True,
     transparent=True,
     dark=(graphics.STAIRS_UP, (0, 0, 100), (50, 50, 150)),
-    light=(graphics.STAIRS_UP, (255, 255, 255), (200, 180, 50)),
+    light=(graphics.STAIRS_UP, color.white, (200, 180, 50)),
 )
 
 entry_stairs = new_tile(
     walkable=True,
     transparent=True,
     dark=(graphics.STAIRS_DOWN, (0, 0, 100), (50, 50, 150)),
-    light=(graphics.STAIRS_DOWN, (255, 255, 255), (200, 180, 50)),
+    light=(graphics.STAIRS_DOWN, color.white, (200, 180, 50)),
 )
 
