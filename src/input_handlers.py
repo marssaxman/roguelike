54a26be880b3393baa1e6d0909a5210a31ee0d00
Mars Saxman <github@marssaxman.com>
2025-05-16 12:08:14 -0700

    remove old inventory handlers now that the new one is finished

--- src/input_handlers.py
+++ src/input_handlers.py
@@ -310,93 +310,6 @@ class LevelUpEventHandler(AskUserEventHandler):
         return None
 
 
-class InventoryEventHandler(AskUserEventHandler):
-    """This handler lets the user select an item.
-
-    What happens then depends on the subclass.
-    """
-
-    TITLE = "<missing title>"
-
-    def on_render(self, console: tcod.console.Console) -> None:
-        """Render an inventory menu, which displays the items in the inventory, and the letter to select them.
-        Will move to a different position based on where the player is located, so the player can always see where
-        they are.
-        """
-        super().on_render(console)
-        number_of_items_in_inventory = len(self.engine.player.inventory.items)
-
-        width = len(self.TITLE) + 4
-        height = number_of_items_in_inventory + 2
-        if height <= 3:
-            height = 3
-
-        if self.engine.player.x <= (console.width // 2):
-            x = console.width - width - 1
-        else:
-            x = 1
-        y = 1
-
-
-        console.draw_frame(
-            x=x,
-            y=y,
-            width=width,
-            height=height,
-            title=self.TITLE,
-            clear=True,
-            fg=(255, 255, 255),
-            bg=(0, 0, 0),
-        )
-
-        if number_of_items_in_inventory > 0:
-            for i, item in enumerate(self.engine.player.inventory.items):
-                item_key = chr(ord("a") + i)
-                is_equipped = self.engine.player.equipment.item_is_equipped(item)
-
-                item_string = f"({item_key}) {item.name}"
-
-                if is_equipped:
-                    item_string = f"{item_string} (E)"
-
-                console.print(x + 1, y + i + 1, item_string)
-        else:
-            console.print(x + 1, y + 1, "(Empty)")
-
-
-    def ev_keydown(self, event: tcod.event.KeyDown)  -> Optional[ActionOrHandler]:
-        player = self.engine.player
-        key = event.sym
-        index = key - tcod.event.KeySym.a
-
-        if 0 <= index <= 26:
-            try:
-                selected_item = player.inventory.items[index]
-            except IndexError:
-                self.engine.message_log.add_message("Invalid entry.", color.invalid)
-                return None
-            return self.on_item_selected(selected_item)
-        return super().ev_keydown(event)
-
-    def on_item_selected(self, item: Item)  -> Optional[ActionOrHandler]:
-        """Called when the user selects a valid item."""
-        raise NotImplementedError()
-
-
-class InventoryActivateHandler(InventoryEventHandler):
-    """Handle using an inventory item."""
-
-    TITLE = "Select an item to use"
-
-    def on_item_selected(self, item: Item)  -> Optional[ActionOrHandler]:
-        if item.consumable:
-            # Return the action for the selected item.
-            return item.consumable.get_action(self.engine.player)
-        elif item.equippable:
-            return actions.EquipAction(self.engine.player, item)
-        else:
-            return None
-
 class InventoryHandler(AskUserEventHandler):
     def __init__(self, engine: Engine):
         super().__init__(engine)
@@ -468,14 +381,6 @@ class InventoryHandler(AskUserEventHandler):
         console.print(x + 1, y + height - 3, "A to use, D to drop.", fg = (255, 255, 255))
 
 
-class InventoryDropHandler(InventoryEventHandler):
-    """Handle dropping an inventory item."""
-
-    TITLE = "Select an item to drop"
-
-    def on_item_selected(self, item: Item)  -> Optional[ActionOrHandler]:
-        """Drop this item."""
-        return actions.DropItem(self.engine.player, item)
 
 class SelectIndexHandler(AskUserEventHandler):
     """Handles asking the user for an index on the map."""
@@ -629,10 +534,6 @@ class MainGameEventHandler(EventHandler):
         elif key == tcod.event.KeySym.z:
             action = PickupAction(player)
 
-        elif key == tcod.event.KeySym.x:
-            return InventoryActivateHandler(self.engine)
-        elif key == tcod.event.KeySym.d:
-            return InventoryDropHandler(self.engine)
         elif key == tcod.event.KeySym.c:
             return CharacterScreenEventHandler(self.engine)
         elif key == tcod.event.KeySym.SLASH:
@@ -646,9 +547,6 @@ class MainGameEventHandler(EventHandler):
 
         # No valid key was pressed
         return action
-    def ev_mousebuttondown(self, event):
-        return InventoryHandler(self.engine)
-        x, y = event.tile
 
 class GameOverEventHandler(EventHandler):
     def on_quit(self) -> None:
